00cde46093bdd1793335cd6f4dde6d377bea6220
gbenga <gbenga.adesanya@konga.com>
2017-01-06 08:52:20 +0100

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const app = express();
+const port = process.env.PORT || 3000;
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'abs');
@@ -27,6 +28,6 @@ app.get('/bad', (req, res) => {
 });
 });
 
-app.listen(3000, () => {
-    console.log('server running on port 3000');
+app.listen(port, () => {
+    console.log(`server running on port ${port}`);
 });
